2ed244471e08036b30dd4cb0272587c58d5aa15b
adeelwebdev0 <adeelwebdev0@gmail.com>
2026-06-13 09:49:16 +0500

    complete authentication

--- dist/backend/controllers/userControllers.js
+++ dist/backend/controllers/userControllers.js
@@ -64,4 +64,44 @@ const logoutCurrentUser = asyncHandler(async (req, res) => {
     });
     res.status(200).json({ message: "Logout succesfuly" });
 });
-export { createUser, loginUser, logoutCurrentUser };
+// geting all users
+const getAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+// getting current user file
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        });
+    }
+    else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user?._id);
+    if (user) {
+        user.username = req.body.username || user.username;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    }
+    else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+export { createUser, loginUser, logoutCurrentUser, getAllUsers, getUserProfile, updateCurrentUserProfile, };
--- dist/backend/routes/userRoutes.js
+++ dist/backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
-import { createUser, loginUser, logoutCurrentUser, } from "../controllers/userControllers.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers, getUserProfile, updateCurrentUserProfile, } from "../controllers/userControllers.js";
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { userSchema } from "../validations/Auth/createUserValidation.js";
 import { loginSchema } from "../validations/Auth/loginUserValidation.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 const router = express.Router();
-router.route("/").post(validate(userSchema), createUser);
+router
+    .route("/")
+    .post(validate(userSchema), createUser)
+    .get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/loginUser", validate(loginSchema), loginUser);
 router.post("/logout", logoutCurrentUser);
+router
+    .route("/profile")
+    .get(authenticate, getUserProfile)
+    .put(authenticate, updateCurrentUserProfile);
 export default router;
